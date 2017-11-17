426a0382a7eeea8cd5889572f20a5610eb7d8643
Adam Parkin <adam.parkin@bambora.com>
2017-11-16 18:48:21 -0800

    Refactor spare test

--- bisect_proj/bowling.py
+++ bisect_proj/bowling.py
@@ -11,9 +11,12 @@ class Game(object):
         score = 0
         frame_index = 0
         for frame in range(10):
-            if self._rolls[frame_index] + self._rolls[frame_index + 1] == 10:
+            if self._is_spare(frame_index):
                 score += 10 + self._rolls[frame_index + 2]
             else:
                 score += self._rolls[frame_index] + self._rolls[frame_index + 1]
             frame_index += 2
         return score
+
+    def _is_spare(self, frame_index):
+        return self._rolls[frame_index] + self._rolls[frame_index + 1] == 10
